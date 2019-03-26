3c5aba463fdd9a1a2f3bd9f3eba269d5954b0a62
justinezahiri <justinezahiri@gmail.com>
2019-03-26 21:31:56 +0100

    implemented homepage

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,27 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../images/EatlocalLogo.png";
 
 class HomePage extends React.Component {
   render() {
     return (
       <div className="homepage">
-        <h1>Eatlocal</h1>
-        <p>
-          You are what you eat
-          <br />
-          You are who you meet
-        </p>
-
-        <Link
-          className="btn signup"
-          to="/register"
-          style={{ textDecoration: "none" }}
-        >
-          Sign up
-        </Link>
-        <Link className="btn" to="/login" style={{ textDecoration: "none" }}>
-          Log in
-        </Link>
+        <div className="container">
+          <img src={logo} alt="logo" />
+          <p>
+            You are what you eat
+            <br />
+            You are who you meet
+          </p>
+          <div className="content">
+            <Link
+              className="btn signup"
+              to="/register"
+              style={{ textDecoration: "none" }}
+            >
+              Sign up
+            </Link>
+            <Link
+              className="btn"
+              to="/login"
+              style={{ textDecoration: "none" }}
+            >
+              Log in
+            </Link>
+          </div>
+        </div>
       </div>
     );
   }
