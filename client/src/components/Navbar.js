be7eb72638238e5e08c54a44d93cc0a43a95da88
justinezahiri <justinezahiri@gmail.com>
2019-03-28 11:07:31 +0100

    implementing dropdown navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,21 +1,36 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import pin from "../images/Eatlocal pin.png";
+import dropdownIcon from "../images/30x30.png";
 
 class Navbar extends Component {
   render() {
     return (
-      <div className="Navbar">
-        <Link to="/">
-          <img src={pin} alt="pin" />
-        </Link>
-        <Link to="/register" className="navbar-link">
-          Signup
-        </Link>
-        <Link to="/login" className="navbar-link">
-          Login
-        </Link>
-      </div>
+      <nav className="Navbar" role="navigation">
+        <ul>
+          <li>
+            <Link to="/">
+              <img src={pin} alt="pin" />
+            </Link>
+          </li>
+          <li>
+            <img src={dropdownIcon} alt="dropdown-icon" />
+
+            <ul class="dropdown">
+              <li>
+                <Link to="/register" className="navbar-link">
+                  Signup
+                </Link>
+              </li>
+              <li>
+                <Link to="/login" className="navbar-link">
+                  Login
+                </Link>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
     );
   }
 }
