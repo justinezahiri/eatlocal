6828f2432ed927003b41bdccf58c8b0aebe6d1c7
justinezahiri <justinezahiri@gmail.com>
2019-03-30 16:04:35 +0100

    implementing profile page integration

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,10 +1,37 @@
 import React, { Component } from "react";
+import Navbar from "./Navbar.js";
 
 class Profile extends Component {
   render() {
     return (
       <div>
-        <p>ton profil</p>
+        <Navbar />
+        <div className="page-content">
+          <div className="content">
+            <div className="profile-card">
+              <h2>Your details</h2>
+              <h3>Username</h3>
+              <h3>Email</h3>
+              <h3>Phone</h3>
+            </div>
+
+            <div className="profile-card">
+              <h2>Your next events</h2>
+              <ul>
+                <li>By: Marwan Sam</li>
+                <li>Beyrouth</li>
+                <li>Lunch</li>
+                <li>3 guests</li>
+              </ul>
+              <ul>
+                <li>By: Fatene Tannous</li>
+                <li>Beyrouth</li>
+                <li>Dinner</li>
+                <li>3 guests</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
