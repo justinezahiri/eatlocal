39e110273057d3fe966e0dd6e18da7c161f412ce
justinezahiri <justinezahiri@gmail.com>
2019-04-19 14:36:10 +0200

    for heroku deployment

--- client/src/components/EventMap.js
+++ client/src/components/EventMap.js
@@ -40,11 +40,6 @@ class EventMap extends Component {
     let icon = L.icon({
       iconUrl: "/images/pinblue.svg",
       iconSize: [38, 95]
-      //iconAnchor: [22, 94]
-      // popupAnchor: [-3, -76],
-      // shadowUrl: 'my-icon-shadow.png',
-      // shadowSize: [68, 95],
-      // shadowAnchor: [22, 94]
     });
 
     return (
